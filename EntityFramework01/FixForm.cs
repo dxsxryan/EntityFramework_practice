14f7be3b49951a86b03ac12a7d89958a2dba7fb0
dxsxryan <dxsxryan@gmail.com>
2023-02-28 13:59:49 +0800

    加入專案檔案。

---
+++ EntityFramework01/FixForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework01
+{
+    public partial class FixForm : Form
+    {
+        public FixForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
